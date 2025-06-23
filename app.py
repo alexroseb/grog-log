45f84f26adeab40089c868222f1407ea5b21832a
Alex Brenon-Hodas <abrenon3@gmail.com>
2025-06-23 12:58:56 -0400

    Add templating and basic drinks page

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, session, json, request, redirect, flash
 import sqlite3
 from setup_db import *
 
 app = Flask(__name__)
-con = sqlite3.connect("grog-log.db")
-cur = con.cursor()
 
-make_drinks()
+make_drinks_db()
+make_users_db()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/<id>")
+def drink_info(id):
+    con = sqlite3.connect("grog-log.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM drinks WHERE id = ?", id)
+    drink_info = res.fetchall()
+    print(drink_info)
+    if len(drink_info)>0:
+        ingredients = drink_info[0][2].split(",")
+        return render_template("drink_details.html", id=drink_info[0][0], name=drink_info[0][1], ingredients=ingredients)
+    else:
+        return render_template("drink_details.html", id=id, name="No drink found", ingredients=[])
--- setup_db.py
+++ setup_db.py
@@ -1,13 +1,13 @@
 import sqlite3
 import csv
 
-con = sqlite3.connect("grog-log.db")
-cur = con.cursor()
+con_setup = sqlite3.connect("grog-log.db")
+cur_setup = con_setup.cursor()
 
-def make_drinks():
-    cur.execute("DROP TABLE IF EXISTS drinks;")
-    con.commit()
-    cur.execute("""CREATE TABLE IF NOT EXISTS drinks(
+def make_drinks_db():
+    cur_setup.execute("DROP TABLE IF EXISTS drinks;")
+    con_setup.commit()
+    cur_setup.execute("""CREATE TABLE IF NOT EXISTS drinks(
                     id INTEGER PRIMARY KEY,
                     name TEXT NOT NULL UNIQUE,
                     ingredients TEXT
@@ -17,16 +17,14 @@ def make_drinks():
         reader = csv.reader(csvfile)
         for row in reader:
             data.append(row)
-    cur.executemany("INSERT INTO drinks VALUES(?, ?, ?)", data)
-    res = cur.execute("SELECT id, name FROM drinks")
-    print(res.fetchall())
-    con.commit()
+    cur_setup.executemany("INSERT INTO drinks VALUES(?, ?, ?)", data)
+    con_setup.commit()
 
-def make_users():
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+def make_users_db():
+    cur_setup.execute("""CREATE TABLE IF NOT EXISTS users(
                     username TEXT PRIMARY KEY,
                     email TEXT NOT NULL UNIQUE,
                     password TEXT NOT NULL,
                     drink_log TEXT
                 ); """)
-    con.commit()
+    con_setup.commit()
